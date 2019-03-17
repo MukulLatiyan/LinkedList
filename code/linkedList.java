ad2f7768a594fee003e01f33d4a90f433f0aa1b0
Mukul Latiyan <subhanshu.25197@gmail.com>
2019-03-17 21:58:53 +0530

    Update linkedList.java

--- code/linkedList.java
+++ code/linkedList.java
@@ -509,6 +509,22 @@ private Node reverseLinkedList(Node head){
 	return head;
 }
 
+// swap list nodes in pairs
+public ListNode swapPairs(ListNode head) {
+       ListNode dummy = new ListNode(0);
+       dummy.next = head;
+       ListNode current = dummy;
+        while (current.next != null && current.next.next != null) {
+          ListNode first = current.next;
+          ListNode second = current.next.next;
+          first.next = second.next;
+          current.next = second;
+          current.next.next = first;
+          current = current.next.next;
+        }
+        return dummy.next;
+    }
+
 // oddEven Linked list
 private Node OddEvenLinkedList(Node head){
 	if(head == null) return null;
